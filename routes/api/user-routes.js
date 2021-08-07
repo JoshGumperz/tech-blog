b3390b9837918799546c5b8bd396d7410d2fb542
Josh Gumperz <joshg3023@gmail.com>
2021-08-06 21:58:04 -0700

    still trying to fix heroku

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User } = require('../../models/randomname');
 
 
 router.get('/login', (req, res) => {
--- routes/home-routes.js
+++ routes/home-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User} = require('../models');
+const { Post, User} = require('../models/randomname');
 
 router.get('/', async (req, res) => {
   try {
