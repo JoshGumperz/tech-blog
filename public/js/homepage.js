cc60f2175be60fa495d8d61e828ea6d2ae3ce164
Josh Gumperz <joshg3023@gmail.com>
2022-03-23 15:07:14 -0700

    made styling adjustments to the sidebar

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -2,17 +2,19 @@ const loginButton = $(".login-btn")
 const newPostButton = $("#new-post-btn")
 const posts = $(".post-title-text")
 const yourPosts = $(".your-posts-btn")
-// const homeButton = $("#home-btn")
-
 // toggle sidebar on and off
 const sidebar = $("#sidebar")
+const overlay =  $("#sidebar-overlay")
 const toggleSidebarOff = () => {
   sidebar.removeClass("sidebar").addClass("sidebar-hidden")
+  overlay.removeClass("sidebar-overlay").addClass("sidebar-hidden")
 }
 const toggleSidebarOn = () => {
   sidebar.removeClass("sidebar-hidden").addClass("sidebar")
+  overlay.removeClass("sidebar-hidden").addClass("sidebar-overlay")
 }
 $(".mobile-icon").click(toggleSidebarOn)
+overlay.click(toggleSidebarOff)
 sidebar.click(toggleSidebarOff)
 
 // homeButton.on("click", function(event){
